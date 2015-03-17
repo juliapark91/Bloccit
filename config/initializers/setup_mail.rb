4e03b3155f99cce2ecece9aa3473af66701e6653
Julia Park <juliapark91@gmail.com>
2015-03-16 17:29:56 -0700

    added gmail again

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development? #if Rails.env.development? will stop Rails from readin
     address:        'smtp.gmail.com',
     port:           '587',
     authentication: :plain,
-    password:       'juliabloctest@gmail.com',
-    user_name:      'julia94403', 
+    user_name:       'juliabloctest@gmail.com',
+    password:      'julia94403', 
     domain:         'gmail.com',
     enable_starttls_auto: true
   }
