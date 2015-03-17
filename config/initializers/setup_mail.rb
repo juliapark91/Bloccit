fbcdc4e235433d52f8fbdcf0fa4da0011e560bdb
Julia Park <juliapark91@gmail.com>
2015-03-16 17:21:16 -0700

    removed sendgrid username and used gmail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 if Rails.env.development? #if Rails.env.development? will stop Rails from reading these mail settings in Production, and block Production email confirmation. We'll fix this together.
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:        'smtp.sendgrid.net',
+    address:        'smtp.gmail.com',
     port:           '587',
     authentication: :plain,
-    password:       ENV['SENDGRID_PASSWORD'],
-    user_name:      ENV['SENDGRID_USERNAME'], #we use environment variables to not reveal sensitive information
-    domain:         'heroku.com',
+    password:       ENV['juliabloctest@gmail.com'],
+    user_name:      ENV['julia94403'], #we use environment variables to not reveal sensitive information
+    domain:         'gmail.com',
     enable_starttls_auto: true
   }
 end
