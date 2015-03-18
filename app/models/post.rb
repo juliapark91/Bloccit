e820472da2498774f05f0ab23779b30b9f05d8e1
Julia Park <juliapark91@gmail.com>
2015-03-18 15:24:54 -0700

    Associate and neste topics and posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+  belongs_to :topic
 
-  scope :ordered_by_title, -> { order('posts.title ASC') }
-  scope :ordered_by_reverse_created_at, -> { order('posts.created_at DESC') }
+  default_scope { order('created_at DESC')}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
- require 'faker'
+require 'faker'
  
 # Create Users
  5.times do
@@ -12,6 +12,15 @@
  end
  users = User.all
 
+# Create Topics
+15.times do
+  Topic.create!(
+    name:         Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
+
 # Note: by calling `User.new` instead of `create`,
 # we create an instance of User which isn't immediately saved to the database.
  
@@ -20,15 +29,16 @@
  
 # The `save` method then saves this User to the database.
 
- # Create Posts
- 50.times do
-   Post.create!(
-      user: users.sample,
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
+# Create Posts
+50.times do
+  Post.create!(
+    user: users.sample,
+    topic: topics.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+  )
+end
+posts = Post.all
  
  # Create Comments
  100.times do
@@ -39,13 +49,35 @@
    )
  end
  
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-  email: 'juliapark91@yahoo.com',
+# Create an admin user
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+admin.skip_confirmation!
+admin.save!
+
+# Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
+)
+moderator.skip_confirmation!
+moderator.save!
+ 
+# Create a member
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+member.skip_confirmation!
+member.save!
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
