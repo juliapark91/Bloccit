d7709415f6793f9066c64d2597f251452f005bd9
Julia Park <juliapark91@gmail.com>
2015-03-19 09:19:05 -0700

    Complete interlude assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     authorize @post
 
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
@@ -42,4 +42,10 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :body)
 end
