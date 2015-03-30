c70fa450e495a0b621f20106998c470b2c2a5c3c
Julia Park <juliapark91@gmail.com>
2015-03-30 13:37:17 -0700

    Complete voting assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
+  after_create :create_vote
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   #validates :topic, presence: true
@@ -28,5 +30,11 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
 
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 end
