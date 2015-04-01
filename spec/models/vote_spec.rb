3f45f88466cd7d63ae4992613465e8d0a17d15dd
Julia Park <juliapark91@gmail.com>
2015-03-31 18:28:38 -0700

    Completed mocking with rspec checkpoint

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -18,7 +18,7 @@ describe Vote do
 
   describe 'after_save' do
     it "calls 'Post#update_rank' after save" do
-      post = associated_post
+      include TestFactories
       vote = Vote.new(value: 1, post: post)
       expect(post).to receive(:update_rank)
       vote.save
