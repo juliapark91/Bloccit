e8a25262abbfed9dce89fb1f49a75d76721bb7b5
Julia Park <juliapark91@gmail.com>
2015-04-01 17:03:46 -0700

    Complete another interlude assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,18 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+
+  def up_vote_link_classes(post)
+    vote = current_user.voted(post)
+    arrow = "glyphicon glyphicon-chevron-up"
+    arrow += "#{@voted}" if vote && vote.up_vote?
+    arrow
+  end
+
+  def down_vote_link_classes(post)
+    vote = current_user.voted(post)
+    arrow = "glyphicon glyphicon-chevron-down"
+    arrow += "#{@voted}" if vote && vote.down_vote?
+    arrow
+  end
 end
