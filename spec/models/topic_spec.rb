c798fbea7340d4a1614a4101d0e04f48fafe509e
Julia Park <juliapark91@gmail.com>
2015-04-03 21:06:54 -0700

    Added missing example

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Topic do
-  describe "scopes" do
+describe "scopes" do
 
     before do
       @public_topic = Topic.create # default is public_topic
@@ -18,7 +18,14 @@ describe Topic do
       it "returns a relation of all private topics" do
         expect(Topic.privately_viewable).to eq( [@private_topic] )  
       end
-      
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = true
+        expect(Topic.visible_to(user)).to eq (Topic.all)
+      end
+
       it "returns only public topics if user is nil" do
         unless @user.present?
           expect(Topic.publicly_viewable).to eq ([@public_topic])
